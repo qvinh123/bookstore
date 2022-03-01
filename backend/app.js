84eca6bf2fb6698ac9c5b2d996f1ef4d80eb2e2c
qvinh123 <60300240+qvinh123@users.noreply.github.com>
2022-03-02 03:19:50 +0700

    Fixed appjs bugs

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,7 @@ const cors = require('cors')
 const path = require('path')
 
 const app = express();
-dotenv.config({ path: "backend/config/config.env" })
+if (process.env.NODE_ENV !== 'PRODUCTION') require('dotenv').config({ path: "backend/config/config.env" })
 
 app.use(express.json({ limit: "50mb" }))
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
