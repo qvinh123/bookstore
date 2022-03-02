216ee1881a12676780e5ad525781e356325e80d7
qvinh123 <60300240+qvinh123@users.noreply.github.com>
2022-03-02 22:38:36 +0700

    fixed

--- backend/redis_connect.js
+++ backend/redis_connect.js
@@ -5,6 +5,11 @@ let redis_client
 
 if (process.env.REDISTOGO_URL) {
     redis_client = redis.createClient(process.env.REDISTOGO_URL);
+
+    var rtg = redis.parse(process.env.REDISTOGO_URL);
+    var redis = redis.createClient(rtg.port, rtg.hostname);
+
+    redis.auth(rtg.auth.split(":")[1]);
 } else {
     redis_client = redis.createClient();
 }
